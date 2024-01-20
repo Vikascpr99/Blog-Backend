33fa905b24eeab8b0149aff03f2f0a53c922e86d
Vikascpr99 <vikascpr99@gmail.com>
2024-01-20 13:46:47 +0530

    Backend for Blogging Application

---
+++ Blog-Backend/src/test/java/com/blogBackend/Blog/Backend/BlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogBackend.Blog.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
